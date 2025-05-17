7e4e5b94897a125495235e61aa882e4760e39ca9
MuhammadAmin02 <muhammadamininto011@gmail.com>
2025-05-17 15:08:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("welcome")
+
+
+
+let a = prompt ("Ismingiz kiriting")
+let b = prompt ("Familiyangiz kiriting")
+let c = prompt (" Yoshingizni kiriting")
+let d = prompt ("Yashash adressingiz kiriting")
+let e = prompt ("Jinsingizni kiriting")
+
+
+console.log(`${a} \n ${b} \n ${c} \n ${d} \n ${e} `)
